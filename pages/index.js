f5ae2b43de37a47c027a520a4d205c3c388156f9
Felix <81396251+adyelson@users.noreply.github.com>
2023-08-07 22:59:42 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1> Olá visitante, seja bem vindo ao meu site profissonal!!! </h1>
+function Home() {
+  return <h1> Olá visitante, seja bem vindo ao meu site profissonal!!! </h1>;
+}
+
+function teste1() {
+  console.log("carro azul.");
 }
 
 export default Home;

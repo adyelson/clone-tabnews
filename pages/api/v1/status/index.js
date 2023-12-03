a97db4d9ebda91e04a953e84b2c8398c4410effa
Felix <81396251+adyelson@users.noreply.github.com>
2023-12-03 00:31:18 +0000

    status atualizado com versao max used connections do

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ msg: "Excelente!" });
+  const updatedAt = new Date().toISOString();
+
+  const porstgresVersion = await database.query("SELECT version();");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const usedConnections = await database.query(
+    "select count(*) from pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: porstgresVersion.rows[0].version,
+    max_connections: maxConnections.rows[0].max_connections,
+    used_connections: usedConnections.rows[0].count,
+  });
 }
 
 export default status;

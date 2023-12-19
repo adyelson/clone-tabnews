d7b3565176d13db671d98776e50df44c53fa89a8
Felix <81396251+adyelson@users.noreply.github.com>
2023-12-19 02:35:56 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,25 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const porstgresVersion = await database.query("SELECT version();");
-  const maxConnections = await database.query("SHOW max_connections;");
-  const usedConnections = await database.query(
-    "select count(*) from pg_stat_activity;",
-  );
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
 
+  const usedConnections = await database.query({
+    text: "select count(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: porstgresVersion.rows[0].version,
-    max_connections: maxConnections.rows[0].max_connections,
-    used_connections: usedConnections.rows[0].count,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        used_connections: usedConnections.rows[0].count,
+      },
+    },
   });
 }
 

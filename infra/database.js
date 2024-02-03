e2dae02cc2cf5bd949ddcd05961d2ec9f968f78b
Felix <81396251+adyelson@users.noreply.github.com>
2024-02-03 15:54:37 +0000

    makes `database` client more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,22 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credenciais do postgress:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
 
     return result; //rever a posição
   } catch (err) {
     console.error(err ? err.stack : result.rows[0].message);
+    throw error;
   } finally {
     await client.end();
   }

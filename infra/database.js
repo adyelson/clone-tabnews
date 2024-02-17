8db8e9fa797b0123a10a15128f5a61d0930f7a8c
Felix <81396251+adyelson@users.noreply.github.com>
2024-02-17 23:20:28 +0000

    add more robust ssl configuration

--- infra/database.js
+++ infra/database.js
@@ -7,31 +7,31 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-  console.log("Credenciais do postgress:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
     await client.connect();
     const result = await client.query(queryObject);
-
     return result; //rever a posição
-  } catch (err) {
-    console.error(err ? err.stack : result.rows[0].message);
+  } catch (error) {
+    console.error(error);
     throw error;
   } finally {
     await client.end();
   }
-  // const result = await client.query(queryObject);
-  // await client.end();
 }
 
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}

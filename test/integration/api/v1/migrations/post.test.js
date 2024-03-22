b785d7451b6c21fcf66aee25a98ba1bff3732ce2
Felix <81396251+adyelson@users.noreply.github.com>
2024-03-22 00:00:50 +0000

    ajuste migrations

--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,10 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -8,4 +15,5 @@ test("GET to /api/v1/migrations should return 200", async () => {
   console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
